b72fa7c862c49d84ec210b829768a6abccac20e7
MaiDao99 <68932625+MaiDao99@users.noreply.github.com>
2020-09-07 23:18:29 +0700

    MongLV: call API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+  console.log(data);
+  console.log(data.length);
+  useEffect(() => {
+    axios.get("http://localhost:2121/todo").then(res => res.data).then((result) => {
+      setData(result);
+    }).catch(err => console.log(err))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header"> */}
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* </header> */}
+      {data.length > 0 && data.map(item => (
+        <div>Todo: {item.todo}</div>
+      ))}
     </div>
   );
 }
