6ff9711d4b5a5320c385048715846cc138c67d12
MaiDao99 <68932625+MaiDao99@users.noreply.github.com>
2020-09-09 17:25:44 +0700

    MongLV: setting project

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+/**
+ * Copyright 2020 present, Lê Văn Mong.
+ * All rights reserved.
+ * @author Mong Le Van
+ * @email: levanmong.dola.99@gmail.com
+ * @student-code: 68DCHT20091
+ * @university: UTT (Đại học Công Nghệ Giao Thông Vận Tải)
+ */
+'use strict';
 import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import axios from 'axios';
@@ -11,17 +20,17 @@ function App() {
     axios.get("http://localhost:2121/todo").then(res => res.data).then((result) => {
       setData(result);
     }).catch(err => console.log(err))
-  }, [])
+  }, []);
 
   return (
-    <div className="App">
-      {/* <header className="App-header"> */}
+      <div className="App">
+        {/* <header className="App-header"> */}
         <img src={logo} className="App-logo" alt="logo" />
-      {/* </header> */}
-      {data.length > 0 && data.map(item => (
-        <div>Todo: {item.todo}</div>
-      ))}
-    </div>
+        {/* </header> */}
+        {data.length > 0 && data.map(item => (
+            <div>Todo: {item.todo}</div>
+        ))}
+      </div>
   );
 }
 
