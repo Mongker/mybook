87b8671de11672d7558d3065c42aa7acab7afbc0
MaiDao99 <68932625+MaiDao99@users.noreply.github.com>
2020-10-08 22:17:42 +0700

    Upload file sử dụng Antd

--- src/components/demo/todo/todoApp.jsx
+++ src/components/demo/todo/todoApp.jsx
@@ -7,30 +7,76 @@
  * @university: UTT (Đại học Công Nghệ Giao Thông Vận Tải)
  */
 
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
-import '../../../App.css';
+// import React, {useEffect, useState} from 'react';
+// import axios from 'axios';
+// import '../../../App.css';
+//
+// function TodoApp() {
+//     const [data, setData] = useState([]);
+//     console.log(data);
+//     console.log(data.length);
+//     useEffect(() => {
+//         axios.post("http://localhost:1999/api/catalog", {
+//             "name": "Mong",
+//             "description": "oki"
+//         }).then(res => res.data).then((result) => {
+//         }).catch(err => console.log(err))
+//     }, []);
+//
+//     return (
+//         <div className="App">
+//             <h1>Mong đẹp trai</h1>
+//             {/*{data.length > 0 && data.map(item => (*/}
+//             {/*    <div>Todo: {item.todo}</div>*/}
+//             {/*))}*/}
+//         </div>
+//     );
+// }
+//
+// export default React.memo(TodoApp);
 
-function TodoApp() {
-    const [data, setData] = useState([]);
-    console.log(data);
-    console.log(data.length);
-    useEffect(() => {
-        axios.post("http://localhost:1999/api/catalog", {
-            "name": "Mong",
-            "description": "oki"
-        }).then(res => res.data).then((result) => {
-        }).catch(err => console.log(err))
-    }, []);
+import React from 'react';
+import {Upload} from 'antd';
 
+const props = {
+    name: 'file',
+    action: 'http://localhost:1999/api/file/upload',
+    multiple: true,
+    onStart(file) {
+        console.log('onStart', file, file.name);
+    },
+    onSuccess(ret) {
+        console.log('onSuccess', ret);
+    },
+    onError(err) {
+        console.log('onError', err);
+    },
+    beforeUpload(file, fileList) {
+        console.log(file, fileList);
+        return new Promise(resolve => {
+            console.log('start check');
+            setTimeout(() => {
+                console.log('check finshed');
+                resolve(file);
+            }, 3000);
+        });
+    },
+};
+
+const Test = () => {
     return (
-        <div className="App">
-            <h1>Mong đẹp trai</h1>
-            {/*{data.length > 0 && data.map(item => (*/}
-            {/*    <div>Todo: {item.todo}</div>*/}
-            {/*))}*/}
+        <div
+            style={{
+                margin: 100,
+            }}
+        >
+            <div>
+                <Upload {...props}>
+                    <a>Test</a>
+                </Upload>
+            </div>
         </div>
     );
-}
+};
 
-export default React.memo(TodoApp);
+export default Test;
