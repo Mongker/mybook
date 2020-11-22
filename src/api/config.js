c8b4a290e3ff0152d26ac1c6a5731f73c211b054
MaiDao99 <68932625+MaiDao99@users.noreply.github.com>
2020-11-23 01:10:26 +0700

    vercel server

--- src/api/config.js
+++ src/api/config.js
@@ -15,7 +15,7 @@
 'use strict';
 
 export const URL_API = {
-  local: 'http://660914137334.ngrok.io/api/',
+  local: 'https://server-mybook.vercel.app/api/',
   // local: 'http://192.168.20.102:1999/api/',
   // local: 'http://localhost:1999/api/',
   ngrok: 'https://b66cd0c7d917.ngrok.io/api/',
